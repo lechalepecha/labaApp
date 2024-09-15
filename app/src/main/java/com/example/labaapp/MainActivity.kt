3765ba9302ee20c75f05827ff8f9a6b068f99f85
aleks <aleksejserakov45@gmail.com>
2024-09-15 16:54:39 +0700

    вместо textinputlayot появилось поле Number

--- app/src/main/java/com/example/labaapp/MainActivity.kt
+++ app/src/main/java/com/example/labaapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.labaapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +20,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    fun WriteMonth(){
-
+    fun WriteMonth(view: View){
+        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
     }
 
 }
